82e507b202b8771060ac08c9f4dd829536216489
Warren Schweigert <prime@zzzzim.uconnect.utah.edu>
2016-10-05 11:05:05 -0600

    edited files

--- A5_stopWatch.pro
+++ A5_stopWatch.pro
@@ -13,8 +13,10 @@ TEMPLATE = app
 
 
 SOURCES += main.cpp\
-        mainwindow.cpp
+        mainwindow.cpp \
+    stopwatchtimer.cpp
 
-HEADERS  += mainwindow.h
+HEADERS  += mainwindow.h \
+    stopwatchtimer.h
 
 FORMS    += mainwindow.ui
--- stopwatchtimer.h
+++ stopwatchtimer.h
@@ -1,12 +1,23 @@
 #ifndef STOPWATCHTIMER_H
 #define STOPWATCHTIMER_H
+#include <QObject>
+#include <QTimer>
 
 
 class StopWatchTimer : public QObject
 {
     Q_OBJECT
+private:
+    QTime time;
+    QTimer timer;
 public:
     explicit StopWatchTimer(QObject *parent = 0);
+
+    void start();
+    void stop();
+    void clear();
+
+    string formatNumbers(int);
 
 signals:
 
